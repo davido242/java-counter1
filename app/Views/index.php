14129c299fda08c54de6a1ceb10c9c0e00f990eb
David042 <Davidsarka242@gmail.com>
2021-05-25 22:05:57 -0700

    Routes and Controller setup

--- app/Views/index.php
+++ app/Views/index.php
@@ -2,8 +2,25 @@
 <html>
 <head>
   <title>Java Script Counter!</title>
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/style.css');?>">
 </head>
 <body>
+  <nav>
+    <h2>CountMe</h2>
+  </nav>
+  <div class="content-wrap">
+    <div class="cont1">
+      <label>-</label>
+    </div>
+    <div class="cont1 white">
+      <label>-7</label>
+    </div>
+    <div class="cont1 green">
+      <label>+</label>
+    </div>
+    
+  </div>
+
 
 </body>
 </html>
